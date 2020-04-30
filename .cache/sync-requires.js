f080e8063d1ccc477e8f9b60126a51835e1ea778
vsasvipul0605 <vsasvipul@gmail.com>
2020-04-30 08:56:38 +0530

    add .cache

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Vipul\\my-coronavirus-map\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Vipul\\my-coronavirus-map\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Vipul\\my-coronavirus-map\\src\\pages\\index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\Vipul\\my-coronavirus-map\\src\\pages\\page-2.js")))
+}
+
